67fb5bd67f86e133772c0dee518d83d9f8100b0f
Treator <alex-zub@yandex.ru>
2020-03-03 13:49:37 +0500

    6 тест

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleApp2
             b = false;
             if (b) Console.WriteLine("He выполняется.");// Результатом выполнения оператора отношения
                                                         // является логическое значение.
-            Console.WriteLine("10 > 9 равно " + (10 > 9));
+            Console.WriteLine("10 > 6 равно " + (10 > 9));
         }
     }
 }
